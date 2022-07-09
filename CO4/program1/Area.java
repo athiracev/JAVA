7cea2f7e7fb1c85678379bf1d26bede26a77ecf4
ATHIRA P <94682674+athiracev@users.noreply.github.com>
2022-07-09 20:05:17 +0530

    Update Area.java

--- CO4/program1/Area.java
+++ CO4/program1/Area.java
@@ -4,7 +4,7 @@ public class Area{
     public static void main(String[] args) {
         Figures s = new Figures();
 
-        System.out.println("Area of Rectangle:"+s.rectangleArea(5,4));
+        System.out.println("Area of Rectangle:"+s.rectangleArea(8,6));
         System.out.println("Area of Triangle:"+s.triangleArea(7.0,3.0));
         System.out.println("Area of Square:"+s.squareArea(4));
         System.out.println("Area of Circle:"+s.circleArea(5.0));
